168cde7ab11369f68c50f739808c55297368e685
keith <keith@keithflower.org>
2013-07-04 23:49:18 -0700

    Initial commit.

---
+++ abg_clojure/project.clj
@@ -0,0 +1,8 @@
+(defproject abg_clojure "0.1.0-SNAPSHOT"
+  :description "Simple ABG interpretation in Clojure"
+  :url "github.com/keithflower"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths      ["src/abg_clojure"]
+  :java-source-paths ["src/java"])
---
+++ abg_clojure/test/abg_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns abg-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [abg-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
